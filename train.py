97f30616d19ccf1f4d088f88dc6aaab5556ae77b
Ubaada <theubaada@gmail.com>
2024-11-04 07:38:09 +1300

    add training and utils script

---
+++ train.py
@@ -0,0 +1,224 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from torch.utils.data import Dataset
+from tokenizers import Tokenizer, trainers, models, pre_tokenizers, normalizers
+from torch.nn.utils.rnn import pad_sequence
+import wandb  
+import kagglehub
+from tokenizers import Tokenizer
+import os
+import pandas as pd
+
+from model import Transformer
+from utils import create_tokenizer, generate_text, load_last_checkpoint
+
+# ========================================================
+# Download dataset and/or set path
+# ========================================================
+path = kagglehub.dataset_download("mohamedlotfy50/wmt-2014-english-german")
+
+train_file = path + "/wmt14_translate_de-en_train.csv"
+val_file = path + "/wmt14_translate_de-en_validation.csv"
+test_file = path + "/wmt14_translate_de-en_test.csv"
+
+# ========================================================
+# Initalise tokenizer
+# ========================================================
+_MAX_LEN = 512 # for training
+print("Loading Tokenizer...")
+tokenizer_pth='wmt14_de_en_tokenizer2.json'
+if not os.path.exists(tokenizer_pth):
+    create_tokenizer(tokenizer_pth, train_file) # create new tokenizer
+
+tokenizer = Tokenizer.from_file(tokenizer_pth)
+tokenizer.enable_padding(pad_id=0, pad_token="[PAD]")
+tokenizer.enable_truncation(max_length=_MAX_LEN)
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+print(tokenizer.encode("this is my cat").tokens)
+
+# ========================================================
+# Dataset Loaders
+# ========================================================
+
+def process_batch(batch, tokenizer, device):
+    # batch is a list of tuples (en, de)
+    # tokenizing collectively for padding
+    en_obj = tokenizer.encode_batch([x[0] for x in batch], add_special_tokens=True)
+    en = torch.tensor([x.ids for x in en_obj]).to(device,non_blocking=True)
+    de_obj = tokenizer.encode_batch([x[1] for x in batch], add_special_tokens=True)
+    de = torch.tensor([x.ids for x in de_obj]).to(device, non_blocking=True)
+    en_padding_mask = torch.tensor([x.attention_mask for x in en_obj]).to(device,non_blocking=True)
+
+    #prepare for training
+    de_input = de[:, :-1]  # remove the last token
+    de_target = de[:, 1:]  # remove the [BOS] token
+    return {
+        "en": en,
+        "de_input": de_input,
+        "de_target": de_target,
+        "en_padding_mask": en_padding_mask
+    }
+
+class WMT14Dataset(Dataset):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = pd.read_csv(file_path, lineterminator="\n")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+        en = "[BOS] " + row["en"] + " [EOS]" # encoder input, not sure if we need BOS and EOS
+        de = "[BOS] " + row["de"] + " [EOS]" # decoder input
+        return en, de
+
+print("Loading Datasets...")
+train_dataset = WMT14Dataset(train_file)
+val_dataset = WMT14Dataset(val_file)
+test_dataset = WMT14Dataset(test_file)
+
+
+
+# ========================================================
+# Model Init (base model from paper)
+# ========================================================
+N = 6
+dmodel = 512
+dff = 2048
+h = 8
+dk = dmodel // h
+enc_vocab_size = 37000
+dec_vocab_size = 37000
+dropout = 0.1
+
+model = Transformer(N, N, dmodel, h, enc_vocab_size, dec_vocab_size, dff, dropout)
+#model = torch.compile(model, mode="reduce-overhead")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+print("Initialised Model. Params:", sum(p.numel() for p in model.parameters()))
+
+
+# ========================================================
+# Training
+# ========================================================
+
+print("Starting training...")
+wandb.init(project="scratch-transformer")
+# Or resume from a previous run id
+#wandb.init(project="scratch-transformer", id="<run-id>", resume="must")
+checkpoint_folder = "./model_weights/"
+
+optimizer = optim.Adam(model.parameters(), lr=0.0001, foreach=True)
+criterion = nn.CrossEntropyLoss(ignore_index=0).to('cpu')
+
+# Training parameters
+torch.set_float32_matmul_precision("medium")  # Precision of matrix multiplication
+start_epoch = load_last_checkpoint(model, checkpoint_folder)
+num_epochs = 25
+batch_size = 16
+accumulation_steps = 8  # Number of batches to accumulate gradients
+
+data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: process_batch(x, tokenizer, device))
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=lambda x: process_batch(x, tokenizer, device))
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=lambda x: process_batch(x, tokenizer, device))
+
+for epoch in tqdm(range(start_epoch+1, num_epochs), desc="Epochs"):
+    model.train()
+    total_loss = 0.0
+    optimizer.zero_grad()  # Zero gradients before starting accumulation
+
+    batch_iterator = tqdm(data_loader, desc=f"Training Epoch {epoch}", leave=True)
+    
+    for step, batch in enumerate(batch_iterator):
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
+            outputs = model(
+                dec_x=batch["de_input"],
+                enc_x=batch["en"],
+                enc_padding_mask=batch["en_padding_mask"]
+            )["logits"]
+            loss = criterion(outputs.view(-1, dec_vocab_size), batch["de_target"].reshape(-1))
+
+        # For printing avg loss in the epoch. Nothing to do with grad accum
+        total_loss += loss.item()
+        
+        # Scale the loss by accumulation_steps
+        loss = loss / accumulation_steps
+        loss.backward()
+        
+        # Update tqdm description to show current batch loss
+        batch_iterator.set_postfix(batch_loss=loss.item() * accumulation_steps)
+        
+        # Perform optimizer step every accumulation_steps
+        if (step + 1) % accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            wandb.log({"batch_loss": loss.item() * accumulation_steps, "epoch": epoch})
+
+        # print the sample translations
+        if (step % 20_000)==0:
+            s1= generate_text(model, tokenizer, "this is my cat", 10)
+            s2 = generate_text(model, tokenizer, "The quick brown fox jumps over the lazy dog, swiftly avoiding the deep, mysterious forest that lies ahead.",40)
+            print(s1)
+            print(s2 + "\n")
+            wandb.log({"example_sentence_1": s1, "example_sentence_2": s2})
+
+    
+    # Perform an optimizer step for remaining gradients
+    if (step + 1) % accumulation_steps != 0:
+        optimizer.step()
+        optimizer.zero_grad()
+    
+    average_loss = total_loss / len(data_loader)
+    wandb.log({"epoch_loss": average_loss, "epoch": epoch + 1})
+    print(f"Epoch {epoch}, Average Loss: {average_loss:.4f}")
+
+    # -------------------------------
+    # Validation Phase   (per epoch)
+    # -------------------------------
+    model.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for val_batch in val_loader:
+            outputs = model(
+                dec_x=val_batch["de_input"],
+                enc_x=val_batch["en"],
+                enc_padding_mask=batch["en_padding_mask"]
+            )["logits"]
+            loss = criterion(outputs.view(-1, dec_vocab_size), val_batch["de_target"].reshape(-1))
+            val_loss += loss.item()
+
+    val_loss /= len(val_loader)
+    wandb.log({"val_loss": val_loss, "epoch": epoch})
+    print(f"Epoch {epoch}, Validation Loss: {val_loss:.4f}")
+    
+    # Save the model checkpoint and upload it to wandb
+    model_path = f"{checkpoint_folder}/transformer_epoch_{epoch}.pt"
+    torch.save(model.state_dict(), model_path)
+    if (epoch % 5) == 0:
+        wandb.save(model_path)
+
+
+
+
+# ========================================
+# Test Phase (after all epochs)
+# ========================================
+model.eval()
+test_loss = 0.0
+with torch.no_grad():
+    for test_batch in test_loader:
+        outputs = model(
+            dec_x=test_batch["de_input"],
+            enc_x=test_batch["en"],
+            enc_padding_mask=batch["en_padding_mask"]
+        )["logits"]
+        loss = criterion(outputs.view(-1, dec_vocab_size), test_batch["de_target"].reshape(-1))
+        test_loss += loss.item()
+
+test_loss /= len(test_loader)
+wandb.log({"test_loss": test_loss})
+print(f"Final Test Loss: {test_loss:.4f}")
---
+++ utils.py
@@ -0,0 +1,98 @@
+from tokenizers import Tokenizer, trainers, models, pre_tokenizers, normalizers
+import pandas as pd
+import torch
+import re
+import os
+
+# ========================================================
+# train tokenizer
+# ========================================================
+def create_tokenizer(tokenizer_pth, train_file):
+    def get_training_corpus(train_file):
+        i = 0
+        for chunk in pd.read_csv(train_file, chunksize=1000, usecols=["de", "en"], lineterminator="\n"):
+            # Drop rows with NaN values in either column
+            chunk = chunk.dropna(subset=["de", "en"])
+            # Convert all entries to strings to avoid type errors
+            combined_text = chunk["de"].astype(str).tolist() + chunk["en"].astype(str).tolist()
+            print("Done:", i, "rows", end="\r")
+            i += 1000
+            yield combined_text
+    
+    # Initialize the tokenizer
+    tokenizer = Tokenizer(models.BPE())
+    tokenizer.normalizer = normalizers.NFKC()
+    tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
+    trainer = trainers.BpeTrainer(vocab_size=37000, special_tokens=["[PAD]", "[BOS]", "[EOS]", "[UNK]"])
+    
+    # Train the tokenizer with data yielded from get_training_corpus
+    tokenizer.train_from_iterator(get_training_corpus(train_file), trainer=trainer)
+    tokenizer.save(tokenizer_pth)
+
+
+# ========================================================
+# greedy decoding
+# ========================================================
+def generate_text(model, tokenizer, enc_text, max_len=10):
+    device = next(model.parameters()).device
+    enc_inp = tokenizer.encode(enc_text)
+    enc_ids = torch.tensor(enc_inp.ids).to(device)
+    # ! enc input must also have BOS and EOS tokens
+    if enc_ids[0] != tokenizer.token_to_id("[BOS]"):
+        enc_ids = torch.cat([torch.tensor([tokenizer.token_to_id("[BOS]")]).to(device), enc_ids])
+    if enc_ids[-1] != tokenizer.token_to_id("[EOS]"):
+        enc_ids = torch.cat([enc_ids, torch.tensor([tokenizer.token_to_id("[EOS]")]).to(device)])
+    enc_padding_mask = torch.tensor(enc_inp.attention_mask).to(device)
+
+    # Add batch dimension if needed
+    if len(enc_ids.shape) == 1:
+        enc_ids = enc_ids.unsqueeze(0)
+        enc_padding_mask = enc_padding_mask.unsqueeze(0)
+
+    # Initialize token history with BOS token ID
+    bos_token_id = tokenizer.token_to_id("[BOS]")
+    gen_ids = [bos_token_id]
+    memory = None
+
+    # Start generation loop
+    for i in range(max_len):
+        prev_dec_inputs = torch.tensor(gen_ids).unsqueeze(0).to(device)
+
+        if memory is None:
+            out = model(dec_x=prev_dec_inputs, enc_x=enc_ids, enc_padding_mask=enc_padding_mask)
+            memory = out["memory"]
+        else:
+            out = model(dec_x=prev_dec_inputs, memory=memory, enc_padding_mask=enc_padding_mask)
+
+        # Get logits of the last token
+        logits = out["logits"]
+        # Get probabilities
+        probs = torch.softmax(logits[0, -1, :], dim=-1)
+        # Get the token with the highest probability
+        next_token_id = torch.argmax(probs).item()
+        # Add the token ID to the history
+        gen_ids.append(next_token_id)
+
+        # Check if the next token is EOS token
+        if next_token_id == tokenizer.token_to_id("[EOS]"):
+            break
+
+    print("Generated token IDs:", gen_ids)
+    # Decode the token IDs to get the generated text
+    generated_text = tokenizer.decode(gen_ids)
+    return generated_text
+
+
+# ========================================================
+# Load last check point (if any)
+# ========================================================
+def load_last_checkpoint(model, folder):
+    checkpoints = [f for f in os.listdir(folder) if re.match(r'transformer_epoch_\d+.pt', f)]
+    if not checkpoints:
+        return 0
+    latest = max(checkpoints, key=lambda x: int(re.search(r'\d+', x).group()))
+    model.load_state_dict(torch.load(folder + latest, map_location=torch.device('cpu')))
+    last_epoch = int(re.search(r'\d+', latest).group())
+    print(f"Loaded checkpoint {latest} at epoch {last_epoch}")
+    return last_epoch
+    
